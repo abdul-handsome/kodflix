6e269fc9982bd77d2608b4cd56e82efdd83c8189
Abdul Bello <crownkunle@sikibell.onmicrosoft.com>
2019-06-03 14:51:35 +0100

    increase title size

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to Codeflix. 
+          <h1>Welcome to Codeflix.</h1> 
         </p>
         <a
           className="App-link"
