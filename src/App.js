0d2bfaa25488276abb33debff64e11688ddcf2ec
Abdul Bello <crownkunle@sikibell.onmicrosoft.com>
2019-06-03 16:10:57 +0100

    task 1 completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import movieLogos from './movie01.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <h1>Welcome to Codeflix.</h1> 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <img src = {movieLogos} />
+      <h1>Welcome 2 kodflix</h1>
     </div>
   );
 }
