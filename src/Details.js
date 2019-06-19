fd39f05977be04c1cbb78a0249e973cef35c01a5
Abdul Bello <crownkunle@sikibell.onmicrosoft.com>
2019-06-19 22:19:06 +0100

    Task 13: intro to component state.  now Completed.

--- src/Details.js
+++ src/Details.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
-export default function Details(){
-    return (
-    <h1><Link to='./'>Back</Link><br />welcome to routing</h1>
-    )
+export default class Details extends React.Component {
 
+    constructor(){
+        super();
+        this.state = {
+            welcomeMessage: 'you are wellycome oooo !'
+        };
+    }
+
+    componentDidMount(){
+        setTimeout(() => {
+            this.setState({welcomeMessage: 'welcome to atlanta'});
+           
+        }, 3000);
+    }
+
+    render() { 
+        return (
+            <h1><Link to='./'>Back</Link><br />{this.state.welcomeMessage}</h1>
+        );
+    }
 }
+
